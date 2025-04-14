ad4f3b265e01bc0491cb4196157bf82e6b4e3f67
Vladas Zakrevskis <146100@gmail.com>
2025-04-14 15:58:21 +0300

    version bump

--- build_image.sh
+++ build_image.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
+./build.py
+
 docker login
 docker buildx create --use
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64 -t vladasz/petuh:0.7.6 --push .
+docker buildx build --platform linux/amd64 -t vladasz/petuh:0.8.1 --push .
