d915a3035d7678bdccf9703a12ae45f571cb8ee0
Vladas Zakrevskis <146100@gmail.com>
2025-04-20 01:52:16 +0300

    fixed velykos bugs

--- src/yayko.rs
+++ src/yayko.rs
@@ -3,34 +3,33 @@ use anyhow::bail;
 use std::collections::BTreeMap;
 use teloxide::Bot;
 use teloxide::prelude::{Requester, UserId};
-use teloxide::types::MediaKind;
 use teloxide::types::Message;
+use teloxide::types::MessageEntity;
 use teloxide::types::MessageKind;
+use teloxide::types::{MediaKind, MessageEntityKind};
 use tokio::sync::Mutex;
+use tokio::sync::MutexGuard;
 
 static USER_INFO: Mutex<BTreeMap<UserId, UserInfo>> = Mutex::const_new(BTreeMap::new());
 
 #[derive(Debug, Clone)]
 struct UserInfo {
     id: UserId,
-    username: String,
+    firstname: String,
+    username: Option<String>,
     yayko_count: u64,
 }
 
 pub async fn yayko_command(bot: Bot, msg: Message) -> Result<()> {
     let id = msg.from.as_ref().unwrap().id;
-    let username = msg
-        .from
-        .as_ref()
-        .unwrap()
-        .username
-        .clone()
-        .unwrap_or_else(|| msg.from.as_ref().unwrap().first_name.clone());
+
+    let from = msg.from.as_ref().unwrap();
 
     let mut user = USER_INFO.lock().await;
     let user = user.entry(id).or_insert_with(|| UserInfo {
         id,
-        username: username.to_string(),
+        firstname: from.first_name.clone(),
+        username: from.username.clone(),
         yayko_count: 20,
     });
 
@@ -45,7 +44,7 @@ pub async fn yayko_command(bot: Bot, msg: Message) -> Result<()> {
         msg.chat.id,
         format!(
             "{}, хочешь узнать сколько у тебя яиц? У тебя: {}",
-            user.username, user.yayko_count
+            user.firstname, user.yayko_count
         ),
     )
     .await?;
@@ -57,62 +56,36 @@ pub async fn yayko_strike(bot: Bot, msg: Message) -> Result<()> {
     let mut lock = USER_INFO.lock().await;
 
     let id = msg.from.as_ref().unwrap().id;
-    let current_username = msg
-        .from
-        .as_ref()
-        .unwrap()
-        .username
-        .clone()
-        .unwrap_or_else(|| msg.from.as_ref().unwrap().first_name.clone());
+    let current_username = msg.from.as_ref().unwrap().first_name.clone();
 
-    let current_user = lock
-        .values()
-        .find(|user| user.username == current_username.clone());
+    let from = msg.from.as_ref().unwrap();
 
-    let Some(mut current_user) = current_user.cloned() else {
-        bot.send_message(
-            msg.chat.id,
-            format!(
-                "{} Ну ты и Пятушара!! Еще не зарегестрировался в игре! Сначала напиши /yayko",
-                current_username
-            ),
-        )
-        .await?;
+    let mut current_user = lock
+        .entry(id)
+        .or_insert_with(|| UserInfo {
+            id,
+            firstname: from.first_name.clone(),
+            username: from.username.clone(),
+            yayko_count: 20,
+        })
+        .clone();
 
-        return Ok(());
-    };
+    let target_user = extract_user(&msg, &lock).unwrap();
 
-    let MessageKind::Common(ref common) = msg.kind else {
-        bail!("Message is not a common message: {msg:?}");
-    };
-
-    let MediaKind::Text(ref text) = common.media_kind else {
-        bail!("Message is not a text message: {msg:?}");
-    };
-
-    let text = text.text.clone();
-
-    let target_username = extract_username(&text).unwrap();
-
-    let target_user = lock
-        .values()
-        .find(|user| user.username == target_username.clone())
-        .cloned();
+    dbg!(&current_user);
+    dbg!(&target_user);
 
     let Some(mut target_user) = target_user.clone() else {
         bot.send_message(
             msg.chat.id,
-            format!(
-                "{} Пятушара!! Еще не зарегестрировался в игре! Пусть напишет /yayko сначала.",
-                target_username
-            ),
+            "Этот пятух еще не зарегестрировался в игре! Пусть напишет /yayko сначала.",
         )
         .await?;
 
         return Ok(());
     };
 
-    if target_user.username == current_username {
+    if target_user.firstname == current_username {
         bot.send_message(msg.chat.id, "Хочешь сам себя уебать? Ты шо еблан?")
             .await?;
 
@@ -134,7 +107,7 @@ pub async fn yayko_strike(bot: Bot, msg: Message) -> Result<()> {
             msg.chat.id,
             format!(
                 "Этот пятух {} уже проебал все свои яйца!! Хуле ты доебался?",
-                target_username
+                target_user.firstname
             ),
         )
         .await?;
@@ -146,7 +119,7 @@ pub async fn yayko_strike(bot: Bot, msg: Message) -> Result<()> {
 
     message.push_str(&format!(
         "{} хуярит своим могучим яйцом 💪  этого пятушару: {}\n",
-        current_username, target_username
+        current_username, target_user.firstname
     ));
 
     let win = rand::random::<bool>();
@@ -154,13 +127,13 @@ pub async fn yayko_strike(bot: Bot, msg: Message) -> Result<()> {
     if win {
         message.push_str(&format!(
             "Найййс. {} расхуярил дряхлое яйцо этого еблана {}\n",
-            current_username, target_username
+            current_username, target_user.firstname
         ));
         target_user.yayko_count -= 1;
     } else {
         message.push_str(&format!(
                 "Ахахах {} проебал далбаееебина тупая 🤣. Твое дряхлое яйцо разъебалось в щепки о великое яйцо {} 💪💪💪💪💪\n",
-                current_username, target_username
+                current_username, target_user.firstname
             ),
         );
         current_user.yayko_count -= 1;
@@ -168,33 +141,74 @@ pub async fn yayko_strike(bot: Bot, msg: Message) -> Result<()> {
 
     message.push_str(&format!(
         "У {} теперь: {}. У {}: {}\n",
-        current_username, current_user.yayko_count, target_username, target_user.yayko_count
+        current_username, current_user.yayko_count, target_user.firstname, target_user.yayko_count
     ));
 
     bot.send_message(msg.chat.id, message).await?;
 
     lock.values_mut()
-        .find(|user| dbg!(&user.username) == dbg!(&current_user.username))
+        .find(|user| dbg!(&user.firstname) == dbg!(&current_user.firstname))
         .expect(&format!(
             "User '{}' not found in USER_INFO",
-            current_user.username
+            current_user.firstname
         ))
         .yayko_count = current_user.yayko_count;
 
     lock.values_mut()
-        .find(|user| dbg!(&user.username) == dbg!(&target_user.username))
+        .find(|user| dbg!(&user.firstname) == dbg!(&target_user.firstname))
         .expect(&format!(
             "User '{}' not found in USER_INFO",
-            target_user.username
+            target_user.firstname
         ))
         .yayko_count = target_user.yayko_count;
 
     Ok(())
 }
 
-fn extract_username(text: &str) -> Option<String> {
+fn extract_user(
+    msg: &Message,
+    lock: &MutexGuard<BTreeMap<UserId, UserInfo>>,
+) -> Result<Option<UserInfo>> {
+    let MessageKind::Common(ref common) = msg.kind else {
+        bail!("Message is not a common message: {msg:?}");
+    };
+
+    let MediaKind::Text(ref text_media) = common.media_kind else {
+        bail!("Message is not a text message: {msg:?}");
+    };
+
+    let text = &text_media.text;
+
+    if text.contains("@") {
+        let username = dbg!(extract_username2(text)).unwrap();
+
+        let user = lock
+            .values()
+            .find(|user| user.username == Some(username.clone()))
+            .cloned();
+
+        return Ok(user);
+    }
+
+    let entity = text_media.entities.first().unwrap();
+
+    let MessageEntityKind::TextMention { ref user } = entity.kind else {
+        bail!("MessageEntityKind::TextMention: {msg:?}");
+    };
+
+    let target_fn = user.first_name.clone();
+
+    let user = lock
+        .values()
+        .find(|user| user.firstname == target_fn)
+        .cloned();
+
+    return Ok(user);
+}
+
+fn extract_username2(text: &str) -> Option<String> {
     let at_pos = text.find('@')?;
     let rest = &text[at_pos + 1..];
-    let username = rest.split_whitespace().next()?;
+    let username = rest.split_whitespace().next()?; // takes only the mention (in case of "@name extra words")
     Some(username.to_string())
 }
