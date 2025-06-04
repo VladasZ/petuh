007db1b9d91be33c379edd6bd32b3f0af6d48fa6
Vladas Zakrevskis <146100@gmail.com>
2025-06-04 20:09:54 +0300

    dock with cross

--- build_image.py
+++ build_image.py
@@ -4,6 +4,8 @@ import subprocess
 import platform
 import sys
 
+builder_name = "petuh_builder"
+
 
 def run(cmd, check=True):
     print(f"Running: {cmd}")
@@ -12,21 +14,22 @@ def run(cmd, check=True):
 
 def main():
     try:
-        run("./build.py")
+        run("cargo install cross --git https://github.com/cross-rs/cross")
+        run("cross build -p glavpetuh --release")
 
         local_registry = "192.168.0.201:30500"
-        image_name = f"{local_registry}/petuh:0.11.5"
+        image_name = f"{local_registry}/petuh:0.11.6"
 
         arch = platform.machine()
         os_name = platform.system()
 
         if os_name == "Linux" and arch == "x86_64":
             print("Building directly with docker (native x86_64 Linux)...")
+            
             run(f"docker build -t {image_name} .")
             run(f"docker push {image_name}")
         else:
             print("Cross-building with docker buildx...")
-            builder_name = "petuh_builder"
 
             run(f"docker buildx create --name {builder_name} --use")
             run("docker buildx inspect --bootstrap")
@@ -34,6 +37,7 @@ def main():
             run(f"docker buildx rm {builder_name}")
 
     except subprocess.CalledProcessError as e:
+        run(f"docker buildx rm {builder_name}")
         print(f"Error during execution: {e}")
         sys.exit(1)
 
