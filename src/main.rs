849e15f16bce2d96e4cf70aa4733c860bedfcb5a
Vladas Zakrevskis <146100@gmail.com>
2025-07-10 21:56:39 +0300

    added sentry

--- src/main.rs
+++ src/main.rs
@@ -424,6 +424,18 @@ async fn main() -> Result<()> {
     pretty_env_logger::init();
 
     dotenv::dotenv().ok();
+
+    let _guard = sentry::init((
+        std::env::var("SENTRY_LINK")?,
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            // Capture user IPs and potentially sensitive headers when using HTTP server integrations
+            // see https://docs.sentry.io/platforms/rust/data-management/data-collected for more info
+            send_default_pii: true,
+            ..Default::default()
+        },
+    ));
+
     log::info!("Starting Telegram bot...");
 
     let bot = Bot::from_env();
